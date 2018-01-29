a8f0ec8a25193ae4997be4fa28b77c2fca680767
1−10 谷 <tani@1-10.com>
2018-01-29 09:20:56 +0900

    [update]es6で記載できるよう環境準備＆初期レンダリング

---
+++ three/three006/src/babel/entry.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+
+const canvas = document.getElementById('canvas');
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+const axes = new THREE.AxesHelper(20);
+
+const planeGeometry = new THREE.PlaneGeometry(60, 20);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+// const cubeGeometry;
+// const cubeMaterial;
+// const cube;
+// 
+// const sphereGeometry;
+// const sphereMaterial;
+// const sphere;
+
+const init = () => {
+  renderer.setClearColor(new THREE.Color(0xeeeeee));
+  renderer.setSize(WIDTH, HEIGHT);
+
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.position.set(15, 0, 0);
+
+  camera.position.set(-30, 40, 30);
+
+  scene.add(axes);
+  scene.add(plane);
+  
+  camera.lookAt(scene.position);
+  render();
+}
+
+const render = () => {
+  renderer.render(scene, camera);
+}
+
+init();
