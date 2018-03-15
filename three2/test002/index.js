40e5bf76188d852b5609fbb51d62588fdb561cdd
1−10 谷 <tani@1-10.com>
2018-03-15 14:04:46 +0900

    [update]meshの回転

--- three2/test002/index.js
+++ three2/test002/index.js
@@ -5,6 +5,7 @@ const camera = new THREE.PerspectiveCamera(50, WIDTH/HEIGHT, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('canvas')
 });
+let cube, sphere;
 let x_position = 0
 let z_position = 0
 
@@ -55,14 +56,14 @@ const setLight = () => {
 const createMesh =  () => {
   const sphereGeomtry = new THREE.SphereGeometry(4, 20, 20)
   const sphereMaterial = new THREE.MeshLambertMaterial({color: 0xff0000})
-  const sphere = new THREE.Mesh(sphereGeomtry, sphereMaterial)
+  sphere = new THREE.Mesh(sphereGeomtry, sphereMaterial)
   sphere.position.set(10, 5, 0)
   sphere.castShadow = true
   scene.add(sphere)
 
   const cubeGeomtry = new THREE.BoxGeometry(4, 4, 4)
   const cubeMaterial = new THREE.MeshLambertMaterial({color: 0x0000ff})
-  const cube = new THREE.Mesh(cubeGeomtry, cubeMaterial)
+  cube = new THREE.Mesh(cubeGeomtry, cubeMaterial)
   cube.position.set(-20, 15, 20)
   cube.castShadow = true
   scene.add(cube)
@@ -78,8 +79,22 @@ const moveCamera = () => {
   z_position++
 }
 
+const moveMesh = () => {
+  cube.rotation.set(
+    Math.sin(x_position * Math.PI / 180),
+    0,
+    Math.cos(z_position * Math.PI / 180)
+  )
+  cube.position.set(
+    10 * Math.cos(x_position * Math.PI / 180),
+    15,
+    30 * Math.sin(z_position * Math.PI / 180)
+  )
+}
+
 const render = () => {
   moveCamera()
+  moveMesh()
   renderer.render(scene, camera)
   requestAnimationFrame(render)
 }
