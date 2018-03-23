6de234fc69dc65c4dc256fc3b3abff74b01d0748
1−10 谷 <tani@1-10.com>
2018-03-23 13:59:31 +0900

    [Update]sprite

--- three2/test007/index.js
+++ three2/test007/index.js
@@ -5,28 +5,41 @@ const aspect = WIDTH / HEIGHT
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('canvas')
 })
-renderer.setClearColor(0xeeeeee)
 renderer.setSize(WIDTH, HEIGHT)
+renderer.setClearColor(0xf9f9f9, 1.0)
 
 const scene = new THREE.Scene()
+scene.fog = new THREE.Fog(0xf9f9f9, 200, 300)
 
 const camera = new THREE.PerspectiveCamera(50, aspect, 0.1, 1000)
-camera.position.set(30, 30, 30)
-camera.lookAt(scene.position)
 
-const axes = new THREE.AxesHelper(20)
-scene.add(axes)
+const material = new THREE.SpriteMaterial({
+  map: new THREE.TextureLoader().load('image/shuzo.jpg')
+})
+material.fog = true
 
-const boxGeometry = new THREE.BoxGeometry(5, 5, 5)
-const boxMaterial = new THREE.MeshLambertMaterial({color: 0xff0000})
-const box = new THREE.Mesh(boxGeometry, boxMaterial)
-scene.add(box)
+for(let i=0; i<1000; i++) {
+  const sprite = new THREE.Sprite(material)
+  sprite.position.set(
+    500 * (Math.random() - 0.5),
+    100 * Math.random() - 40,
+    500 * (Math.random() - 0.5)
+  )
+  sprite.scale.set(10, 10, 10)
+  scene.add(sprite)
+}
 
-const directionalLight = new THREE.DirectionalLight()
-directionalLight.position.set(15, 15, 0)
-scene.add(directionalLight)
+const plane = new THREE.GridHelper(300, 10, 0x888888, 0x888888)
+plane.position.y = -40
+scene.add(plane)
 
 const render = () => {
+  camera.position.set(
+    100 * Math.sin(Date.now() / 2000),
+    50 * Math.sin(Date.now() / 1000) + 60,
+    100 * Math.cos(Date.now() / 2000)
+  )
+  camera.lookAt(new THREE.Vector3(0, 0, 0))
   renderer.render(scene, camera)
   requestAnimationFrame(render)
 }
