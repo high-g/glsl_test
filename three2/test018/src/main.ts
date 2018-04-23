0a1b6183ae46cc1fe60644c89285420c03581e6f
1−10 谷 <tani@1-10.com>
2018-04-23 13:37:26 +0900

    [Update]

--- three2/test018/src/main.ts
+++ three2/test018/src/main.ts
@@ -1,2 +1,16 @@
 import * as THREE from 'three'
 
+export class Main {
+  public scene: THREE.Scene
+  public renderer: THREE.WebGLRenderer
+  public camera: THREE.PerspectiveCamera
+  public containerElement: HTMLElement
+  
+  constructor() {
+    this.setup()
+  }
+
+  private setup(): void {
+
+  }
+}
