8b728b811716f88917bb35e266bb13382ed8ba07
1−10 谷 <tani@1-10.com>
2018-04-26 11:59:01 +0900

    [Update]

--- three2/test018/src/main.ts
+++ three2/test018/src/main.ts
@@ -1,12 +1,29 @@
 import * as THREE from 'three'
 import Earth from './objects/Earth'
+import CityPoint from './objects/CityPoint'
+import CityLine from './objects/CityLine'
+import TrackballControls from 'three-trackballcontrols'
 
 export class Main {
   public scene: THREE.Scene
-  public renderer: THREE.WebGLRenderer
   public camera: THREE.PerspectiveCamera
+  public renderer: THREE.WebGLRenderer
+  public controller: TrackballControls
   public containerElement: HTMLElement
-  
+
+  public earth: Earth
+  public japan: CityPoint
+
+  public cities: CityPoint[]
+  public citiesLine: CityLine[]
+
+  public citiesPoints: number[][] = [
+    [51.2838, 0],
+    [39, -116]
+  ]
+
+  public satellite: CityPoint;
+
   constructor() {
     this.setup()
   }
@@ -15,3 +32,5 @@ export class Main {
 
   }
 }
+
+new Main()
