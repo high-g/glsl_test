b0865e96c5b3b2411768f85505339df08f363f79
tani <tani@1-10.com>
2019-05-10 20:11:40 +0900

    [update]texture

--- three/035/src/main.js
+++ three/035/src/main.js
@@ -14,16 +14,28 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(45, width/height)
 camera.position.set(0,0,1000)
 
-const geometry = new THREE.BoxGeometry(400,400,400)
-const material = new THREE.MeshNormalMaterial()
+//const geometry = new THREE.BoxGeometry(400,400,400)
+//const material = new THREE.MeshNormalMaterial()
+const geometry = new THREE.SphereGeometry(300,30,30)
+const loader = new THREE.TextureLoader()
+const texture = loader.load('img/plutomap1k.jpg')
+
+//const material = new THREE.MeshStandardMaterial({color: 0xff0000})
+const material = new THREE.MeshStandardMaterial({
+  map: texture
+})
+
 const box = new THREE.Mesh(geometry, material)
 scene.add(box)
 
-tick()
+const directionalLight = new THREE.DirectionalLight(0xffffff)
+directionalLight.position.set(1, 1, 1)
+scene.add(directionalLight)
 
-function tick() {
+const tick = () => {
   box.rotation.y += 0.01
-  box.rotation.x += 0.001
   renderer.render(scene, camera)
   requestAnimationFrame(tick)
 }
+
+tick()
