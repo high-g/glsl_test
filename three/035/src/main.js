3d29adea3d6104cac76512ccf3ce4c531b55a704
tani <tani@1-10.com>
2019-05-13 19:44:36 +0900

    [update]mesh追加関数、複数mesh

--- three/035/src/main.js
+++ three/035/src/main.js
@@ -3,37 +3,68 @@ import * as THREE from 'three'
 const width = 960
 const height = 540
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById('canvas')
-})
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(width, height)
+let renderer
+let scene
+let camera
+let arrMesh = []
 
-const scene = new THREE.Scene()
+const init = () => {
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('canvas')
+  })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(width, height)
 
-const camera = new THREE.PerspectiveCamera(45, width/height)
-camera.position.set(0,0,1000)
+  scene = new THREE.Scene()
 
-//const geometry = new THREE.BoxGeometry(400,400,400)
-//const material = new THREE.MeshNormalMaterial()
-const geometry = new THREE.SphereGeometry(300,30,30)
+  camera = new THREE.PerspectiveCamera(45, width/height)
+  camera.position.set(0,0,2200)
 
-//const material = new THREE.MeshStandardMaterial({color: 0xff0000})
-const material = new THREE.MeshStandardMaterial({
-  map: new THREE.TextureLoader().load('img/plutomap1k.jpg')
-})
+  arrMesh.push( createSphereMesh(200, 30, 30, 'img/plutomap1k.jpg', [100,100,0]) )
+  arrMesh.push( createSphereMesh(300, 30, 30, 'img/earthcloudmap.jpg', [0,0,0]) )
+  arrMesh.push( createSphereMesh(300, 30, 30, 'img/sunmap.jpg', [650, 0, 0]) )
 
-const box = new THREE.Mesh(geometry, material)
-scene.add(box)
+  setSceneMesh()
 
-const directionalLight = new THREE.DirectionalLight(0xffffff)
-directionalLight.position.set(1, 1, 1)
-scene.add(directionalLight)
+  const directionalLight = new THREE.DirectionalLight(0xffffff)
+  directionalLight.position.set(1, 1, 1)
+  scene.add(directionalLight)
+
+  tick()
+}
+
+const createSphereMesh = (r, ws, hs, path, position) => {
+  const box = new THREE.Mesh(
+    new THREE.SphereGeometry(r,ws,hs),
+    new THREE.MeshStandardMaterial({
+      map: new THREE.TextureLoader().load(path)
+    })
+  )
+
+  if(position) {
+    box.position.set(position[0], position[1], position[2])
+  }
+
+  return box
+}
+
+const setSceneMesh = () => {
+  arrMesh.map((mesh) => {
+    console.log(mesh)
+    scene.add(mesh)
+  })
+}
+
+const meshRandomRotation = () => {
+  arrMesh.map((mesh) => {
+    mesh.rotation.y += 0.01
+  })
+}
 
 const tick = () => {
-  box.rotation.y += 0.01
+  meshRandomRotation()
   renderer.render(scene, camera)
   requestAnimationFrame(tick)
 }
 
-tick()
+init()
