0f5c76c97f103e4cd559968435bbfa6d5f1621f5
tani <tani@1-10.com>
2019-05-13 20:01:29 +0900

    [update]three 調整

--- three/036/src/main.js
+++ three/036/src/main.js
@@ -1,1 +1,23 @@
 import * as THREE from 'three'
+
+const canvas = document.getElementById('canvas')
+const mouse = new THREE.Vector2()
+
+
+const init = () => {
+  canvas.addEventListener('mousemove', handleMouseMove)
+}
+
+const handleMouseMove = (event) => {
+  const element = event.currentTarget;
+
+  const x = event.clientX - element.offsetLeft;
+  const y = event.clientY - element.offsetTop;
+
+  const w = element.offsetWidth
+  const h = element.offsetHeight
+
+  mouse.x = (x/w)
+}
+
+init()
